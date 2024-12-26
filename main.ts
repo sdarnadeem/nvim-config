7a5deded3ee39f6a145fe0cc5de1a21c73fe31c4
Nasyx Nadeem <sdarnadeem@gmail.com>
2024-12-26 22:45:19 +0530

    add github copilot

--- main.ts
+++ main.ts
@@ -1,4 +1,23 @@
-type Props = {
-  name: string;
-  age: number;
-};
+function generateRandomNumber() {
+  return Math.random();
+}
+
+function generateSixDigitOTP() {
+  return Math.floor(100000 + Math.random() * 900000);
+}
+
+function writeASwitchFuntionThatReturnKeyPressedOnALlKeysOnTheKeyboard(
+  key: string,
+) {
+  // return key pressed on the keyboard
+  switch (key) {
+    case "a":
+      return "Lowercase a pressed";
+    case "A":
+      return "Uppercase A pressed";
+    case "1":
+      return "Number 1 pressed";
+  }
+}
+
+generateSixDigitOTP();
